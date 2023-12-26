5e793f9f7e7accb9422dbf22e9af19b8b75e00ee
Terence Burridge <terence@tesandco.co.uk>
2023-12-26 16:36:30 +0000

    0.96-beta - Moved View Code to separate file and out of umbraco resource, refactored migration process + fix some minor RSS validation errors.

---
+++ Migrations/SuperEasyRssPackageMigration.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Options;
+using Umbraco.Cms.Core.Configuration.Models;
+using Umbraco.Cms.Core.IO;
+using Umbraco.Cms.Core.PropertyEditors;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Strings;
+using Umbraco.Cms.Infrastructure.Migrations;
+using Umbraco.Cms.Infrastructure.Packaging;
+
+namespace Our.Umbraco.SuperEasyRss.Migrations
+{
+    public class SuperEasyRssPackageMigration : PackageMigrationBase
+    {
+        private readonly IFileService _fileService;
+
+        public SuperEasyRssPackageMigration(
+            IPackagingService packagingService,
+            IMediaService mediaService,
+            MediaFileManager mediaFileManager,
+            MediaUrlGeneratorCollection mediaUrlGenerators,
+            IShortStringHelper shortStringHelper,
+            IContentTypeBaseServiceProvider contentTypeBaseServiceProvider,
+            IMigrationContext context,
+            IFileService fileService,
+            IOptions<PackageMigrationSettings> packageMigrationsSettings)
+            : base(
+                packagingService,
+                mediaService,
+                mediaFileManager,
+                mediaUrlGenerators,
+                shortStringHelper,
+                contentTypeBaseServiceProvider,
+                context,
+                packageMigrationsSettings)
+        {
+            _fileService = fileService;
+        }
+
+        protected override void Migrate()
+        {
+            var template = _fileService.GetTemplate("rssFeedPage") ?? _fileService.CreateTemplateWithIdentity("RSS Feed Page", "RssFeedPage", null);
+            ImportPackage.FromEmbeddedResource<SuperEasyRssPackageMigration>().Do();
+        }
+    }
+}
---
+++ Migrations/SuperEasyRssPackageMigrationPlan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Umbraco.Cms.Core.Packaging;
+using Umbraco.Cms.Infrastructure.Packaging;
+
+namespace Our.Umbraco.SuperEasyRss.Migrations
+{
+    public class SuperEasyRssPackageMigrationPlan : PackageMigrationPlan
+    {
+        public SuperEasyRssPackageMigrationPlan() : base("Super Easy RSS")
+        {
+        }
+
+        protected override void DefinePlan()
+        {
+            To<SuperEasyRssPackageMigration>(new Guid("fafa5772-03bd-4c5c-89b7-d8f5f8709b7f"));
+        }
+    }
+}
